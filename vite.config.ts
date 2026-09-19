7ceb5070b4a08f20f628b5fe417367a76b1efca9
nkvision2025-spec <nkvision2025@gmail.com>
2026-09-19 20:57:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import {defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/NkTvSetup/',
+  base: '/', // અહીં આપણે સુધારો કર્યો છે (Vercel માટે)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
